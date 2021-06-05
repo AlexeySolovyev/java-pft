6fca404300504aaa74096894cd3269a8c64256dd
Alexey Solovyev <solovevalex@gmail.com>
2021-06-05 15:12:39 -0400

    2 Methods has been added

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -3,7 +3,30 @@ package ru.stqa.pft.sandbox;
 public class MyFirstProgram {
 
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        hello("Alex");
+        hello("Lara");
+        hello("Tim");
+        System.out.println();
+
+        double l = 5;
+        System.out.println("Area of square with side 5 = " + squareArea(l));
+        System.out.println();
+
+        double a = 4;
+        double b = 6;
+        System.out.println("Area of rectangle with sides " + a + " and " + b + " = " + rectangleArea(a, b));
+    }
+
+    public static void hello(String word) {
+        System.out.println("Hello, " + word + "!");
+    }
+
+    public static double squareArea(double l) {
+        return l * l;
+    }
+
+    public static double rectangleArea(double a, double b) {
+        return (a * b);
     }
 }
 
